e4d9e2639e523081872ecd227b852e0097b6b884
Colin Koo <chkoo@cpp.edu>
2017-05-14 20:01:23 -0700

    Secant checkpoint

--- src/FindingRoots.java
+++ src/FindingRoots.java
@@ -9,8 +9,8 @@ public class FindingRoots {
 		equation.add(-11.7);
 		equation.add(17.7);
 		equation.add(-5.0);
-		bisection(0, 0.5, equation, 0.365098);
-
+		//		bisection(0, 0.5, equation, 0.365098);
+		secant(0, 0.5, equation, 0.365098);
 	}
 	// no special checks, assumes root exists between brackets
 	public static void bisection(double a, double b, ArrayList<Double> equation, double trueRoot){
@@ -18,14 +18,14 @@ public class FindingRoots {
 		double prevC = Double.NaN, c, errorA = Double.POSITIVE_INFINITY, errorT = Double.POSITIVE_INFINITY;
 		double fa, fb, fc = Double.NaN;
 		int iterations = 0; 
-		
+
 		while (errorA > 0.01 && iterations < 100){
 			fa = calcEquation(a, equation);
 			fb = calcEquation(b, equation);
 
 			c = (a+b)/2;
 			fc = calcEquation(c, equation);
-			
+
 			if (iterations == 0){
 				prevC = c;
 			}
@@ -34,7 +34,7 @@ public class FindingRoots {
 				prevC = c;
 			}
 			errorT = (Math.abs(trueRoot - c))/trueRoot;
-			
+
 			System.out.println("n: " + iterations
 					+ "\na: " + a + ",\tb: " + b + ",\tc: " + c
 					+"\nfa:\t" + fa + ", fb:\t" + fb + ", fc:\t" + fc
@@ -48,8 +48,12 @@ public class FindingRoots {
 		}
 	}
 	public static void secant(double a, double b, ArrayList<Double> equation, double trueRoot){
-		double start = 0, end = 0;
-		if (a < b){
+		double start = 0, end = 0, next, prevNext = Double.NaN;
+		double fstart, fend, fnext;
+		double errorA = Double.POSITIVE_INFINITY, errorT = Double.POSITIVE_INFINITY;
+		
+		
+		if (Math.abs(calcEquation(a, equation)) < Math.abs(calcEquation(b, equation))){
 			start = a;
 			end = b;
 		}
@@ -57,9 +61,39 @@ public class FindingRoots {
 			start = b;
 			end = a;
 		}
-		
+
+		int iterations = 0;
+		while (iterations < 100 && errorA > 0.01){
+			fstart = calcEquation(start, equation);
+			fend = calcEquation(end, equation);
+
+			next = end - ( (fend*(end-start)) / (fend - fstart) );
+			fnext = calcEquation(next, equation);
+
+			if (iterations == 0){
+				prevNext = next;
+			}
+			else{
+				errorA = Math.abs(next  - prevNext)/next;
+				prevNext = next;
+			}
+			errorT = (Math.abs(trueRoot - next))/trueRoot;
+			System.out.println("n: " + iterations
+					+ "\nstart: " + start + ",\tend: " + end + ",\tnext: " + next
+					+"\nfstart:\t" + fstart + ", fend:\t" + fend + ", fnext:\t" + fnext
+					+ "\nErrorA:\t" + errorA + ", ErrorT:\t" + errorT + "\n");
+
+			if (Math.abs(fend) < Math.abs(fnext)){
+				start = end;
+				end = next;
+			}
+			else{
+				start = next;
+			}
+			++iterations;
+		}
 	}
-	
+
 	public static double calcEquation(double variable, ArrayList<Double> equation){
 		double sum = 0;
 		if (equation.size() != 0){
